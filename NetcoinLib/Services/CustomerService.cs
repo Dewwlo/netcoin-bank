d32aea934aeb816a6bb64dee7dd1e972395d33a1
JochoVoled <johannes@smidelov.com>
2017-11-08 14:47:09 +0100

    Add: CreateCustomer with unit test

--- NetcoinLib/Services/CustomerService.cs
+++ NetcoinLib/Services/CustomerService.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿using NetcoinLib.Models;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace NetcoinLib.Services
 {
-    class CustomerService
+    public class CustomerService
     {
+        private INetcoinRepository repository;
+        public CustomerService(INetcoinRepository repository)
+        {
+            this.repository = repository;
+        }
+        public bool CreateCustomer(string Name, string LegalId, string Area = null, string Address = null, string PostalCode = null)
+        {
+            try
+            {
+                int highId = 0;
+                if (repository.GetCustomers().Count > 0)
+                {
+                    highId = repository.GetCustomers().OrderByDescending(x => x.CustomerId).First().CustomerId;
+                }
+                Customer customer = new Customer
+                {
+                    CustomerId = highId + 1,
+                    LegalId = LegalId,
+                    Name = LegalId,
+                    Area = Area,
+                    Address = Address,
+                    PostalCode = PostalCode,
+                    Accounts = new List<Account>()
+                };
+                // TODO Replace transaction account with real CreateAccount once method is made
+                Account transaction = new Account { Balance = 0, Customer = customer };
+                customer.Accounts.Add(transaction);
+                repository.GetCustomers().Add(customer);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            
+        }
         public void GetCustomer(string search)
         {
 
--- NetcoinUnitTests/CustomerServiceTest.cs
+++ NetcoinUnitTests/CustomerServiceTest.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using NetcoinLib;
+using NetcoinLib.Models;
+using NetcoinLib.Services;
+using NetcoinUnitTests.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
 
 namespace NetcoinUnitTests
 {
-    class CustomerServiceTest
+    public class CustomerServiceTest
     {
+        [Fact]
+        public void CanCreateCustomer()
+        {
+            INetcoinRepository repository = new FakeNetcoinRepository();
+            CustomerService sut = new CustomerService(repository);
+            string legalId = "101112-1234";
+            bool result = sut.CreateCustomer("John Doe", legalId, "Testington", "Main street 1", "12312");
+            Assert.True(result);
+            Customer customer = repository.GetCustomers()[0];
+            Assert.Equal(legalId, customer.LegalId);
+        }
+
         [Fact]
         public void CustomerSearchTest()
         {
