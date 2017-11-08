342a7d73acc11fb73eece58ca5cc41e43f14dbf4
aeklundh <eklundh.albin@gmail.com>
2017-11-08 10:31:04 +0100

    Add basic withdraw implementation

--- NetcoinLib/BankSystem.cs
+++ NetcoinLib/BankSystem.cs
@@ -1,8 +1,30 @@
-﻿using System;
+﻿using NetcoinLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NetcoinLib
 {
     public class BankSystem
     {
+        private readonly INetcoinRepository _netcoinRepository;
+        public BankSystem(INetcoinRepository netcoinRepository)
+        {
+            _netcoinRepository = netcoinRepository;
+        }
+
+        public void WithdrawFromAccount(int accountId, decimal amountToWithdraw)
+        {
+            Account account = _netcoinRepository.GetAccounts().SingleOrDefault(x => x.AccountId == accountId);
+            if (account != null)
+            {
+                if (account.Balance >= amountToWithdraw)
+                    account.Balance = account.Balance - amountToWithdraw;
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
     }
 }
