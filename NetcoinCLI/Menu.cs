fdbf77d4490b72bd82a59b488e3f2eee45880834
Jesper Engdahl <p3pp33ng@gmail.com>
2017-11-08 09:51:23 +0100

    Added a Menu class with two methods.

---
+++ NetcoinCLI/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetcoinCLI
+{
+    public class Menu
+    {
+        public void GetConsoleInput()
+        {
+
+        }
+
+        public void HandleMenuSelection()
+        {
+
+        }
+    }
+}
