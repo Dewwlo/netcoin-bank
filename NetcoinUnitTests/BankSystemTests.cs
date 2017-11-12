9f6383b3aee6955e1f6bc3cda5a9a965b09a6919
Johannes Smidelöv <johannes@smidelov.com>
2017-11-12 11:48:14 +0100

    Add: Unit test for Deposit method

--- NetcoinUnitTests/BankSystemTests.cs
+++ NetcoinUnitTests/BankSystemTests.cs
@@ -29,5 +29,24 @@ namespace NetcoinUnitTests
             Assert.Throws<ArgumentOutOfRangeException>(() => sut.WithdrawFromAccount(1, -1M));
             Assert.Throws<NullReferenceException>(() => sut.WithdrawFromAccount(100, 1M));
         }
+
+        [Fact]
+        public void DepositToAccount()
+        {
+            //Assemble
+            NetcoinRepoRepresentation representation = NetcoinRepositoryUtility.CreateSampleCustomersAndAccounts(5, 0M);
+            INetcoinRepository fakeProvider = new FakeNetcoinRepository();
+            fakeProvider.GetAccounts().AddRange(representation.Accounts);
+            BankSystem sut = new BankSystem(fakeProvider);
+            sut.Initialize();
+
+            //Act
+            sut.DepositToAccount(2, 100M);
+
+            //Act & assert
+            Assert.True(fakeProvider.GetAccounts().Single(x => x.AccountId == 2).Balance == 100);
+            Assert.Throws<ArgumentOutOfRangeException>(() => sut.WithdrawFromAccount(1, -1M));
+            Assert.Throws<NullReferenceException>(() => sut.WithdrawFromAccount(100, 1M));
+        }
     }
 }
