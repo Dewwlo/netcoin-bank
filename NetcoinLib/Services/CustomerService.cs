045e1da1c8153d5a490069464eae069dbd378270
aeklundh <eklundh.albin@gmail.com>
2017-11-13 10:54:47 +0100

    Add limited validation in CreateCustomer() to disallow empty parameters

--- NetcoinLib/Services/CustomerService.cs
+++ NetcoinLib/Services/CustomerService.cs
@@ -28,6 +28,10 @@ namespace NetcoinLib.Services
         }
         public bool CreateCustomer(string Name, string LegalId, string Area, string Address, string PostalCode, string City, string Country, string PhoneNumber)
         {
+            if (String.IsNullOrWhiteSpace(Name) || String.IsNullOrWhiteSpace(LegalId) || String.IsNullOrWhiteSpace(Area) || String.IsNullOrWhiteSpace(PostalCode)
+                || String.IsNullOrWhiteSpace(City) || String.IsNullOrWhiteSpace(Country) || String.IsNullOrWhiteSpace(PhoneNumber))
+                return false;
+
             try
             {
                 int highId = 0;
